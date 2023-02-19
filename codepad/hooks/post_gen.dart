ede1b06d7fce4ed25fe40fbca206f7debb93559f
Randal L. Schwartz <merlyn@stonehenge.com>
2023-02-19 11:32:43 -0800

    0.4.4 - select only web for flutter to save time

--- codepad/hooks/post_gen.dart
+++ codepad/hooks/post_gen.dart
@@ -8,12 +8,23 @@ Future<void> run(HookContext context) async {
   final dart = vars['dart'] as bool;
 
   if (dart) {
-    final result = await Process.runSync('dart', ['pub', 'get'],
+    final result = await Process.runSync(
+        'dart',
+        [
+          'pub',
+          'get',
+        ],
         workingDirectory: snake_name);
     print('stdout is ${result.stdout}');
     print('stderr is ${result.stderr}');
   } else {
-    final result = await Process.runSync('flutter', ['create', '.'],
+    final result = await Process.runSync(
+        'flutter',
+        [
+          'create',
+          '.',
+          '--platforms=web', // typically, we want to create a web app
+        ],
         workingDirectory: snake_name);
     print('stdout is ${result.stdout}');
     print('stderr is ${result.stderr}');
